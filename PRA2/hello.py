7ee559625bbdd3aa654942b6232e2041b1906e2f
li-ruihao <a834878215@gmail.com>
2024-09-26 13:47:11 -0400

    Updated hello.py for example 3-12

--- PRA2/hello.py
+++ PRA2/hello.py
@@ -1,17 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap, Moment
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+import sys
 
 if __name__ == '__main__':
+    print(sys.executable)
     app.run()
